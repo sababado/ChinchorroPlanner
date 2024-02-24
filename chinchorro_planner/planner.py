f05d26ebcc8beaa2973c067fafa66c924625162b
Robert <robert.james.szabo@gmail.com>
2024-02-24 02:11:45 -0700

    Basic UI

--- chinchorro_planner/planner.py
+++ chinchorro_planner/planner.py
@@ -3,15 +3,24 @@ from tkinter import *
 import constant
 
 
+def gradient_formula(x: int):
+    return float(pow(x, 2))
+
+
 class ChinchorroCanvas:
     def __init__(self, canvas, thread_count, thread_length):
         self.canvas = canvas
         self.thread_count = thread_count
         self.thread_length = thread_length
+        self.colors = ["blue", "orange"]
+        self.output = []
 
-    def draw_chinchorro(self, gradient_amt, gradient_pos, gradient_str):
+    def print_output(self):
+        print(self.output)
+
+    def draw_chinchorro(self, gradient_range, gradient_pos, gradient_str):
         padding = 2
-        gradient_amt_percent = gradient_amt / 100
+        gradient_amt_percent = gradient_range / 100
         gradient_pos_percent = gradient_pos / 100
 
         gradient_thread_ct = (gradient_amt_percent * constant.GRADIENT_FULL_PERCENT) * self.thread_count
@@ -19,17 +28,72 @@ class ChinchorroCanvas:
         gradient_starting_thread = color_inflection_thread - (gradient_thread_ct / 2)
         gradient_ending_thread = gradient_starting_thread + gradient_thread_ct
 
+        # how many strength ranges are in the "range"
+        str_in_range = (gradient_ending_thread - gradient_starting_thread) / gradient_str
+        threads_in_str_range = gradient_thread_ct / str_in_range
+        # Add one to the strength range top to fade into the solid color.
+        top_gradient_str_range_number: float = gradient_formula(str_in_range + 1)
+
+        # Loops through the threads while in the gradient
+        j = 0
+        # Tracking which strength range we're in.
+        str_num = 1
+        # Track the thread in each str_num
+        k = 0
+        # track all output
+        self.output.clear()
+        output_counter = 0
+        output_last_color = None
         for i in range(self.thread_count):
             # print(i)
-            # is_in_gradient = gradient_starting_thread <= i <= gradient_ending_thread
-            # if is_in_gradient:
+            is_in_gradient = gradient_starting_thread <= i <= gradient_ending_thread
+            if is_in_gradient:
+                #
+                # x^2, range 0 to gradient thread count, x is the str_num (str_in_range?)
+                # = y, percentage of range will be next color.
+                # use J. Pretend y is 20%
+                # If gradient_str is 20, every 4th thread should turn color.
+                #    j % (gradient_str * y) == 0
 
-            # else:
-            line_color = "blue" if i < color_inflection_thread else "orange"
+                t = gradient_formula(str_num)
+                y: float = t / top_gradient_str_range_number
+                # This tells how many threads in the range should have the next color.
+                threads_to_be_gradientized = round(gradient_str * y)
+                thread_index = (gradient_str / (threads_to_be_gradientized + 1))
+
+                # Increment or reset the in-gradient-thread-index
+                if j >= thread_index:
+                    line_color = self.colors[1]
+                    j = 0
+                else:
+                    line_color = self.colors[0]
+                    j += 1
+
+                # Increment or reset the str_num
+                if k <= threads_in_str_range:
+                    k += 1
+                else:
+                    k = 0
+                    str_num += 1
+
+                print("j-%d gradient_str-%d top-%f t-%f y-%f threads_to_be_gradientized-%f thread_index-%d" % (
+                    j, gradient_str, top_gradient_str_range_number, t, y, threads_to_be_gradientized, thread_index))
+            else:
+                line_color = self.colors[0] if i < color_inflection_thread else self.colors[1]
 
             # Uncomment to show the gradient range
             # line_color = "red" if is_in_gradient else line_color
-            self.canvas.create_line(padding + i, 2, padding + i, 200, fill=line_color)
+            output_counter += 1
+            if output_last_color != line_color:
+                if output_last_color is not None:
+                    self.output.append((output_last_color, output_counter))
+                    output_last_color = line_color
+                    output_counter = 0
+                else:
+                    output_last_color = line_color
+            if i == self.thread_count - 1:
+                self.output.append((output_last_color, output_counter))
+            self.canvas.create_line(padding + i, 2, padding + i, constant.THREAD_LENGTH_CM, fill=line_color)
 
 
 window = Tk()
@@ -40,23 +104,24 @@ print(constant.CANVAS_CONTROLS_HEIGHT)
 canvas_controls = Canvas(window, width=constant.CANVAS_CONTROLS_WIDTH,
                          height=constant.CANVAS_CONTROLS_HEIGHT)
 
-scale_gradient_amt_val = IntVar()
+scale_gradient_range_val = IntVar()
 scale_gradient_pos_val = IntVar()
 scale_gradient_str_val = IntVar()
 
 
 def chinchorro_callback(value=None):
-    chinchorro.draw_chinchorro(scale_gradient_amt_val.get(),
+    chinchorro.draw_chinchorro(scale_gradient_range_val.get(),
                                scale_gradient_pos_val.get(),
                                scale_gradient_str_val.get())
+    chinchorro.print_output()
 
 
-lbl_gradient_amt = Label(canvas_controls, text="Gradient Amount")
-scale_gradient_amt = Scale(canvas_controls, variable=scale_gradient_amt_val,
-                           from_=0, to=100,
-                           command=chinchorro_callback,
-                           orient=HORIZONTAL)
-scale_gradient_amt.set(constant.GRADIENT_AMT)
+lbl_gradient_range = Label(canvas_controls, text="Gradient Range")
+scale_gradient_range = Scale(canvas_controls, variable=scale_gradient_range_val,
+                             from_=1, to=100,
+                             command=chinchorro_callback,
+                             orient=HORIZONTAL)
+scale_gradient_range.set(constant.GRADIENT_AMT)
 
 lbl_gradient_pos = Label(canvas_controls, text="Gradient Position")
 scale_gradient_pos = Scale(canvas_controls, variable=scale_gradient_pos_val,
@@ -67,15 +132,10 @@ scale_gradient_pos.set(constant.GRADIENT_POS)
 
 lbl_gradient_str = Label(canvas_controls, text="Gradient Strength")
 scale_gradient_str = Scale(canvas_controls, variable=scale_gradient_str_val,
-                           from_=10, to=50,
+                           from_=2, to=100,
                            command=chinchorro_callback,
                            orient=HORIZONTAL)
-
-b1 = Button(canvas_controls, text="Display Horizontal",
-            command=chinchorro_callback,
-            bg="yellow")
-
-l1 = Label(canvas_controls)
+scale_gradient_str.set(10)
 
 canvas_controls.pack()
 chinchorro_canvas = Canvas(window, width=constant.CANVAS_CHINCHORRO_WIDTH,
@@ -84,14 +144,12 @@ print(chinchorro_canvas)
 chinchorro = ChinchorroCanvas(chinchorro_canvas, constant.THREAD_COUNT, constant.THREAD_LENGTH_CM)
 chinchorro_callback()
 
-lbl_gradient_amt.pack(anchor=CENTER)
-scale_gradient_amt.pack(anchor=CENTER)
+lbl_gradient_range.pack(anchor=CENTER)
+scale_gradient_range.pack(anchor=CENTER)
 lbl_gradient_pos.pack(anchor=CENTER)
 scale_gradient_pos.pack(anchor=CENTER)
 lbl_gradient_str.pack(anchor=CENTER)
 scale_gradient_str.pack(anchor=CENTER)
-b1.pack(anchor=CENTER)
-l1.pack()
 
 chinchorro_canvas.pack(anchor=CENTER, fill=BOTH)
 
